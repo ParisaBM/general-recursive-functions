f52c7e1f8350b4f4081b7307478b2d2ed3c5edb0
Bijan Betel Miri <17bbm@queensu.ca>
2020-04-26 10:06:26 -0400

    added debug command line option

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"strconv"
+	"fmt"
 )
 
 //these are the signals the phases use to communicate with one another
@@ -33,21 +35,100 @@ var p_to_t chan byte
 var t_to_c chan byte
 
 func main() {
-	//this program will sometimes be run as its executable, and sometimes with go run
-	//hence the filename is assumed to be the last arguement
-	//this feature will be removed in the future
 	//it is beyond the scope of this project to support multi-file codebases
-	filename := os.Args[len(os.Args)-1]
+	//there is an optional second arguement to debug a particular phase
+	//if an integer is given as the fifth arguement, its respective phase
+	//0=scan, 1=parse etc. doesn't output to the next phase and is instead
+	//output to stdout with annotations
+	filename := os.Args[1]
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
+	n := 3 //n is the phase the is being debbuged (if any)
+	if len(os.Args) == 3 {
+		n, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+		//as there are 2 phases with output, n must be 0, or 1
+		if n < 0 || n > 1 {
+			panic("invalid debug phase")
+		}
+	}
 	//next we initialize the channels
 	s_to_p = make(chan byte)
 	p_to_t = make(chan byte)
+	t_to_c = make(chan byte)
 	//then we begin the phases
-	go scan(string(file))
-	go parse()
-	go semantic()
-	<-t_to_c
+	if n >= 0 {
+		go scan(string(file))
+	}
+	if n >= 1 {
+		go parse()
+	}
+	if n >= 2 {
+		go semantic()
+	}
+	if n != 3 {
+		debug(n)
+	}
+}
+
+func debug(n int) {
+	//debug outputs the result of the nth phase
+	//ch is whichever channel it's supposed to be listening to
+	var ch chan byte
+	switch n {
+	case 0:
+		ch = s_to_p
+	case 1:
+		ch = p_to_t
+	}
+	L: for {
+		//there's a bit of code to handle compound tokens, but otherwise it just displays each token
+		switch <- ch {
+		case constant:
+			fmt.Println("constant")
+			fmt.Println(<- ch)
+		case suc:
+			fmt.Println("suc")
+		case proj:
+			fmt.Println("proj")
+			//in the scanner output there are constant markers that are removed by the parser
+			if n==1 {
+				fmt.Println(<- ch)
+				fmt.Println(<- ch)
+			}
+		case comp:
+			fmt.Println("comp")
+			//in the parser, comp is followed by its arity01
+			if n==1 {
+				fmt.Println(<- ch)
+			}
+		case min:
+			fmt.Println("min")
+		case rec:
+			fmt.Println("rec")
+		case identifier:
+			fmt.Println("identifier")
+			fmt.Println(<- ch)
+		case equals:
+			fmt.Println("equals")
+		case open_paren:
+			fmt.Println("open_paren")
+		case close_paren:
+			fmt.Println("close_paren")
+		case comma:
+			fmt.Println("comma")
+		case newline:
+			//the output is grouped by line
+			fmt.Println("newline\n")
+		case err:
+			fmt.Println("err")
+		case end:
+			fmt.Println("end")
+			break L
+		}
+	}
 }
